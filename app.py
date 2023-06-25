aae282bae878a8c9edf39dbebaa3fe6850f5985b
zangyuchen <zangyuchen>
2023-06-25 13:45:16 +0800

    customize my page settings

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        animal = request.form["prompt"]
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
